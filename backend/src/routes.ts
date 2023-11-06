7abc0ae4a09776a7759a5688e6c03c6a8f96cca2
Lucas <lucas.fn4@gmail.com>
2023-11-06 11:49:20 -0300

    Refatoração: mover código para controller

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,55 +1,20 @@
 import { Router } from 'express';
 import { UserController } from './controllers/Users/UsersController';
-import fs from 'fs';
-import {parse} from 'csv-parse';
+import { CSVController } from './controllers/CSV/CSVController';
 import multer from 'multer';
 
 const upload = multer({ dest: '/temp/' });
 
-const routes = Router()
+const routes = Router();
 
+// a gente vai deixar isso assim mesmo?
 routes.get('/', (req, res) => {
   res.send('Hello World! Você está na raiz da API!')
-})
-
-routes.post('/csv', upload.single('file'),(req, res) => {
-    const filePath = req.file?.path;
-    if(!filePath) return res.status(400).send('erro');
-
-    const data = fs.readFileSync(filePath);
-
-    try {
-        parse(data, (err, records) => {
-            if(err) {
-                console.log('erro ao ler dados');
-                return res.status(400).json({res: 'erro'});
-            } 
-
-            const chaves = records.shift();
-            const jsonRetorno = [];
-
-            for(let i = 0; i < chaves.length; i++) {
-                if(chaves[i].includes('ID Discord')) chaves[i] = 'discordID';
-                if(chaves[i].includes('Email')) chaves[i] = 'email';
-            }
-
-            for(let linha of records) {
-                const dados:any = {};
-                for(let i = 0; i < linha.length; i++) {
-                    dados[chaves[i]] = linha[i];
-                }
-                jsonRetorno.push(dados);
-            }
-
-            return res.json(jsonRetorno);
-        }); 
-    } catch(e) {
-        return res.status(400).send('erro');
-    }
 });
 
-routes.post('/user', new UserController().create)
-routes.post('/login', new UserController().login)
-routes.get('/profile', new UserController().getProfile)
+routes.post('/csv', upload.single('file'), new CSVController().importCSV);
+routes.post('/user', new UserController().create);
+routes.post('/login', new UserController().login);
+routes.get('/profile', new UserController().getProfile);
 
 export { routes }
