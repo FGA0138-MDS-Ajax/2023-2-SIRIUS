cf3e9dd9d88bc746caf0702ec343beffcf26d311
Lucas <lucas.fn4@gmail.com>
2023-11-07 11:08:19 -0300

    Pequena refatoração no backend

--- backend/src/controllers/CSV/CSVController.ts
+++ backend/src/controllers/CSV/CSVController.ts
@@ -5,20 +5,22 @@ import { Request, Response } from 'express'
 export class CSVController {
   async importCSV(req: Request, res: Response) {
 
+    // validar entrada de dados
     if(!req.body.fileContents || req.body.fileContents === '') 
         return res.status(400).send('erro: csv vazio')
 
-    const jsonRetorno = [];
 
     const csvContents = req.body.fileContents.replaceAll('"', '').split('\n')
+
+    // tratar as chaves da tabela
     const chaves = csvContents.shift().split(',')
-
-
     for(let i = 0; i < chaves.length; i++) {
       if(chaves[i].includes('ID Discord')) chaves[i] = 'discordID'
       if(chaves[i].includes('Email')) chaves[i] = 'email'
     }
 
+    // formar um json de retorno com os dados da tabela
+    const jsonRetorno = [];
     for(let linha of csvContents) {
       linha = linha.split(',')
       const dados: any = {}
