e5cd1d170582bcbae019ae8bd2617f1254c3cf83
PhMoraiis <liperapltda@gmail.com>
2023-11-07 21:40:37 -0300

    Tipando o controlador CSV

--- backend/src/controllers/CSV/CSVController.ts
+++ backend/src/controllers/CSV/CSVController.ts
@@ -6,31 +6,31 @@ export class CSVController {
   async importCSV(req: Request, res: Response) {
 
     // validar entrada de dados
-    if(!req.body.fileContents || req.body.fileContents === '') 
-        return res.status(400).send('erro: csv vazio')
-
+    if (!req.body.fileContents || req.body.fileContents === '')
+      return res.status(400).send('CSV Vazio')
 
     const csvContents = req.body.fileContents.replaceAll('"', '').split('\n')
 
     // tratar as chaves da tabela
     const chaves = csvContents.shift().split(',')
-    for(let i = 0; i < chaves.length; i++) {
-      if(chaves[i].includes('ID Discord')) chaves[i] = 'discordID'
-      if(chaves[i].includes('Email')) chaves[i] = 'email'
+    for (let i = 0; i < chaves.length; i++) {
+      if (chaves[i].includes('ID Discord')) chaves[i] = 'discordID'
+      if (chaves[i].includes('Email')) chaves[i] = 'email'
     }
 
     // formar um json de retorno com os dados da tabela
-    const jsonRetorno = [];
-    for(let linha of csvContents) {
+    const jsonRetorno = []
+    for (let linha of csvContents) {
       linha = linha.split(',')
-      const dados: any = {}
-
-      for(let i = 0; i < linha.length; i++) {
-        dados[chaves[i]] = linha[i]
+      const dados: { [key: string]: string } = {}
+      for (let i = 0; i < linha.length; i++) {
+        for (let i = 0; i < linha.length; i++) {
+          dados[chaves[i]] = linha[i]
+        }
+        jsonRetorno.push(dados)
       }
-      jsonRetorno.push(dados)
-    }
 
-    return res.json(jsonRetorno)
+      return res.json(jsonRetorno)
+    }
   }
 }
