dfbf2335149eb8741cdf6b1230ca04bba2773b8e
JoaoEduardoP <joaoeduardopereira@yahoo.com.br>
2023-11-12 19:21:44 -0300

    Implementação da US2 (Sorteio de Usuários do Torneio ao Enviar o Arquivo.csv)

--- backend/src/controllers/CSV/CSVController.ts
+++ backend/src/controllers/CSV/CSVController.ts
@@ -9,14 +9,16 @@ export default class CSVController {
       }
 
       const csvContents = this.parseCSV(fileContents)
-
+    
       if (!csvContents) {
         return res.status(400).send('CSV Inválido')
       }
 
-      const chaves = this.extractKeys(csvContents)
-      const jsonRetorno = this.generateJSONData(csvContents, chaves)
-
+      const arrord = this.fillArray(csvContents)
+      const csvRandom = this.arrayToCSV(arrord)
+      const chaves = this.extractKeys(csvRandom)
+      const jsonRetorno = this.generateJSONData(csvRandom, chaves)
+      
       return res.json(jsonRetorno)
     } catch (error) {
       return res.status(500).send('Erro no Processamento do CSV: ' + (error as string))
@@ -40,6 +42,40 @@ export default class CSVController {
       })
     }
     return []
+  }
+
+  public fillArray(csvContents: string[]){
+    const arrayord: string[][] = []
+    csvContents.forEach((linha) =>{
+      const colunas = linha.split(',');
+      arrayord.push(colunas);    
+    });
+    this.shuffleArray(arrayord)
+    return arrayord
+  }
+
+  public shuffleArray(inputArray: string[][]) {
+    inputArray.sort(() => Math.random() - 0.5)
+  } 
+  public arrayToCSV(array: string[][]) {
+    const csvRows = [];
+    this.moveSubarrayToFirstPosition(array,["teamName","inGameName","checkedInAt","userID","ID Discord (Exemplo#1234)","Email de Contato"])
+    for (const row of array) {
+      const csvRow = row.map(value => `${value}`).join(',');
+      csvRows.push(csvRow)
+    }
+    return csvRows;
+  }
+
+  public moveSubarrayToFirstPosition(array: string[][], subarrayToMove: string[]) {
+    const index = array.findIndex(subarray => {
+      return subarray.every((value, i) => value === subarrayToMove[i]);
+    });
+    if (index > -1) {
+      const removedSubarray = array.splice(index, 1)[0];
+      array.unshift(removedSubarray);
+    }
+    return array;
   }
 
   public generateJSONData(csvContents: string[], chaves: string[]): { [key: string]: string }[] {
