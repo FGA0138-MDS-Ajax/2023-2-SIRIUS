1533bd7d1b7693a239aab9d204413f8d978ae0f1
PhMoraiis <liperapltda@gmail.com>
2023-11-29 23:21:14 -0300

    Adicioanndo model de relacionamentos
    Co-authored-by: Lucas Queiroz <lucas.fn4@gmail.com>

--- backend/src/controllers/Grupos/GrupoController.ts
+++ backend/src/controllers/Grupos/GrupoController.ts
@@ -1,45 +1,29 @@
 import { Request, Response } from 'express'
 import { PrismaClient } from '@prisma/client'
-import { PlayerData } from './type'
 
 const prisma = new PrismaClient()
-/**
- * Classe Para Controle dos Métodos dos Grupos.
- */
+
 export class GrupoController {
-  /**
-     * Método para Criação dos Grupos na DataBase.
-     */
   async create(req: Request, res: Response) {
-    const {id, grupos, rodada, idRodada, participante} = req.body
-      
+    const { id, rodada, idRodada, participante } = req.body
+
     const GrupoExists = await prisma.grupos.findUnique({ where: { id } })
-      
+
     if (GrupoExists) {
       return res.status(400).json({ error: 'Grupo already exists!' })
     }
-  
+
     const newGrupo = await prisma.grupos.create({
-      data: {id, grupos, rodada, idRodada, participante},
-    }) 
-  
+      data: { id, rodada, idRodada, participante },
+    })
+
     if (!newGrupo) {
       return res.status(400).send('invalid group')
     }
 
     return res.status(201).json(newGrupo)
 
-  } 
-    
-  /**
-     * Método para Deleção dos Grupos na DataBase.
-     */
-  /* async delete(req: Request, res: Response) {  
-
-    const deleteGrupos = await prisma.grupos.deleteMany({}) 
-
-  }  */
-
+  }
 
   public calcularQuantidadeGrupos(Num_checkin: number): { jogadoresPorGrupo: number[] } {
     let quantidadeGrupos: number[]
