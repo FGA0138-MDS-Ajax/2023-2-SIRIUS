33abd1dbefb0dfc99ff5a18783de77f5fc228671
Lucas <lucas.fn4@gmail.com>
2023-11-30 01:15:29 -0300

    Criação de vários usuários de uma vez
    Co-authored-by: Philipe Morais <liperapltda@gmail.com>
    Co-authored-by: Lucas Meirels <katunerx@gmail.com>

--- backend/src/controllers/Participantes/ParticipantesController.ts
+++ backend/src/controllers/Participantes/ParticipantesController.ts
@@ -6,22 +6,38 @@ const prisma = new PrismaClient()
 export class ParticipantesController {
   async create(req: Request, res: Response) {
 
-    const { id, teamName, inGameName, checkedInAt, idDiscord, email, grupo, grupoId } = req.body
-
+    const { id, teamName, inGameName, checkedInAt, discordID, email } = req.body
     const ParticipanteExists = await prisma.participante.findUnique({ where: { id } })
 
     if (ParticipanteExists) {
-      return res.status(400).json({ error: 'Participante already exists!' })
+      return res.status(400).json({ error: 'O participante já existe.' })
     }
 
     const newParticipante = await prisma.participante.create({
-      data: { id, teamName, inGameName, checkedInAt, idDiscord, email, grupo, grupoId },
+      data: { id, teamName, inGameName, checkedInAt, discordID, email },
     })
 
     if (!newParticipante) {
-      return res.status(400).send('invalid participant')
+      return res.status(400).send('Erro ao criar participante')
     }
 
     return res.status(201).json(newParticipante)
   }
+
+  async createVariosParticipantes(req: Request, res: Response) {
+    const participantesData = req.body.participantes
+
+    try {
+      const createdParticipantes = await prisma.participante.createMany({
+        data: participantesData,
+        skipDuplicates: true,
+      })
+
+      return res.status(201).json(createdParticipantes)
+    } catch (error) {
+      console.error('Error creating participants:', error)
+      return res.status(500).send('Internal Server Error')
+    }
+  }
+  
 }  
