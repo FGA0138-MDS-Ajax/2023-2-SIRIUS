93c02f7239ecd202420f4d155773a149c0c1170a
PhMoraiis <liperapltda@gmail.com>
2023-11-30 01:28:16 -0300

    Removendo funcao Participantes Controller
    Co-authored-by: Lucas Queiroz <lucas.fn4@gmail.com>
    Co-authored-by: Lucas Meireles <katunerx@gmail.com>

--- backend/src/controllers/ParticipanteEmGrupo/ParticipanteEmGrupoController.ts
+++ backend/src/controllers/ParticipanteEmGrupo/ParticipanteEmGrupoController.ts
@@ -23,20 +23,4 @@ export class ParticipantesController {
 
     return res.status(201).json(newParticipante)
   }
-
-  async createVariosParticipantes(req: Request, res: Response) {
-    const participantesData = req.body.participantes
-
-    try {
-      const createdParticipantes = await prisma.participante.createMany({
-        data: participantesData,
-        skipDuplicates: true,
-      })
-
-      return res.status(201).json(createdParticipantes)
-    } catch (error) {
-      console.error('Error creating participants:', error)
-      return res.status(500).send('Internal Server Error')
-    }
-  }
 }  
--- backend/src/controllers/Participantes/ParticipantesController.ts
+++ backend/src/controllers/Participantes/ParticipantesController.ts
@@ -4,26 +4,6 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export class ParticipantesController {
-  async create(req: Request, res: Response) {
-
-    const { id, teamName, inGameName, checkedInAt, discordID, email } = req.body
-    const ParticipanteExists = await prisma.participante.findUnique({ where: { id } })
-
-    if (ParticipanteExists) {
-      return res.status(400).json({ error: 'O participante já existe.' })
-    }
-
-    const newParticipante = await prisma.participante.create({
-      data: { id, teamName, inGameName, checkedInAt, discordID, email },
-    })
-
-    if (!newParticipante) {
-      return res.status(400).send('Erro ao criar participante')
-    }
-
-    return res.status(201).json(newParticipante)
-  }
-
   async createVariosParticipantes(req: Request, res: Response) {
     const participantesData = req.body.participantes
 
