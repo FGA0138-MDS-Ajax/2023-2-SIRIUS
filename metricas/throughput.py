8e3855d50e18adce0affbd0dd10d9f24fad968ea
fabioaletorres <fabioalessandrotorres@gmail.com>
2023-12-04 12:31:24 -0300

    add sprint 6

--- metricas/throughput.py
+++ metricas/throughput.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-sprints = ['01','02','03','04','05']
-tasks = [32,39,42,45,52]
+sprints = ['01','02','03','04','05','06']
+tasks = [32,39,42,45,52,65]
 
 plt.bar(sprints,tasks,color="purple")
 
@@ -10,4 +10,5 @@ plt.xticks(sprints)
 plt.ylabel('Tasks')
 plt.xlabel('Sprints')
 plt.title('Throughput')
+plt.grid(True)
 plt.show()
