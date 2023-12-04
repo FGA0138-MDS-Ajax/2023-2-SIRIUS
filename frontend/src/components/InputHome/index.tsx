3c37e0e45042efb9f8deae605bf40af540813903
PhMoraiis <liperapltda@gmail.com>
2023-12-04 06:44:25 -0300

    Criando tabela e input da home

--- frontend/src/components/InputHome/index.tsx
+++ frontend/src/components/InputHome/index.tsx
@@ -3,6 +3,8 @@ import { Swords } from 'lucide-react'
 import { useState } from 'react'
 import { API } from '../../server/api'
 import { IFormSearchGrupoData } from './type'
+import LoadingSpinner from '../LoadingSpinner'
+import TableGrupos from '../TableGrupos'
 
 const InputHome = () => {
   const [formData, setFormData] = useState<IFormSearchGrupoData>({
@@ -11,6 +13,9 @@ const InputHome = () => {
     nomeTorneio: ''
   })
   const [loading, setLoading] = useState(false)
+  const [formVisible, setFormVisible] = useState(true)
+  const [error, setError] = useState<string | null>()
+  const [data, setData] = useState<any | null>()
 
   const EnumRodada = [
     'UM',
@@ -21,6 +26,11 @@ const InputHome = () => {
   ]
 
   const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+
+    setLoading(true)
+    setFormVisible(false)
+    setError(null)
+
     try {
       event.preventDefault() // Evita o comportamento padrão de enviar o formulário
 
@@ -29,8 +39,15 @@ const InputHome = () => {
       if (data) {
         console.log('Grupo encontrado', data)
         setFormData(data)
+        setData(data)
+        setFormVisible(false)
       }
+      setLoading(false)
+
     } catch {
+      setFormVisible(true)
+      setLoading(false)
+      setError('Erro ao encontrar grupo')
       console.log('Erro ao encontrar grupo')
     }
   }
@@ -42,62 +59,74 @@ const InputHome = () => {
     })
   }
 
+  const ErrorDisplay = () => error && <p className="text-red-500 mt-2 text-center font-bold text-xl">{error}</p>
 
   return (
-    <div className="flex items-center justify-center lg:pb-[14rem] md:pb-[10rem] pb-8 ">
-      <form onSubmit={onSubmit} className="flex flex-col gap-y-8 md:flex-row md:gap-x-6 lg:flex-row lg:gap-x-8 font-caustenBd">
-        <div className="flex flex-col">
-          <label htmlFor="inGameName" className="text-gray-200 mb-6 text-2xl">
-            Nome
-          </label>
-          <input
-            type="text"
-            name="inGameName"
-            id="inGameName"
-            value={formData.inGameName}
-            onChange={handleChange}
-            placeholder="Digite o seu Game Name"
-            className="px-4 h-14 w-96 rounded-lg input text-white text-lg"
-          />
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="torneio" className="text-gray-200 mb-6 text-2xl">
-            Torneio
-          </label>
-          <input
-            type="text"
-            name="nomeTorneio"
-            id="torneio"
-            value={formData.nomeTorneio}
-            onChange={handleChange}
-            placeholder="Digite o nome do Torneio"
-            className="px-4 h-14 w-80 rounded-lg input text-white lg:text-xl text-lg md:texl-lg outline-none"
-          />
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="rodadas" className="text-gray-200 mb-6 text-2xl">Rodada</label>
-          <select
-            id="rodadas"
-            name="numeroRodada"
-            value={formData.numeroRodada}
-            onChange={handleChange}
-            className="px-4 h-14 w-80 rounded-lg input text-white lg:text-xl text-lg md:texl-lg outline-none"
-          >
-            <option value="">Selecione a Rodada</option>
-            {EnumRodada.map((numeroRodada, index) => (
-              <option key={index} value={numeroRodada}>{numeroRodada}</option>
-            ))}
-          </select>
-        </div>
-        <div className="flex items-end justify-center">
-          <button
-            type="submit"
-            className="btn-gradient2 py-4 px-4 rounded-3xl hover:scale-105 duration-300 ease-in-out"
-          >
-            <Swords size={30} strokeWidth={1.5} />
-          </button>
-        </div>
-      </form>
+    <div className="flex flex-col items-center justify-center lg:pb-[14rem] md:pb-[10rem] pb-8 ">
+      {loading && (
+        <LoadingSpinner loading={loading} />
+      )}
+      {formVisible && (
+        <form onSubmit={onSubmit} className="flex flex-col gap-y-8 md:flex-row md:gap-x-6 lg:flex-row lg:gap-x-8 font-caustenBd">
+          <div className="flex flex-col">
+            <label htmlFor="inGameName" className="text-gray-200 mb-6 text-2xl">
+              Nome
+            </label>
+            <input
+              type="text"
+              name="inGameName"
+              id="inGameName"
+              value={formData.inGameName}
+              onChange={handleChange}
+              placeholder="Digite o seu Game Name"
+              className="px-4 h-14 w-96 rounded-lg input text-white text-lg"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="torneio" className="text-gray-200 mb-6 text-2xl">
+              Torneio
+            </label>
+            <input
+              type="text"
+              name="nomeTorneio"
+              id="torneio"
+              value={formData.nomeTorneio}
+              onChange={handleChange}
+              placeholder="Digite o nome do Torneio"
+              className="px-4 h-14 w-80 rounded-lg input text-white lg:text-xl text-lg md:texl-lg outline-none"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="rodadas" className="text-gray-200 mb-6 text-2xl">Rodada</label>
+            <select
+              id="rodadas"
+              name="numeroRodada"
+              value={formData.numeroRodada}
+              onChange={handleChange}
+              className="px-4 h-14 w-80 rounded-lg input text-white lg:text-xl text-lg md:texl-lg outline-none"
+            >
+              <option value="">Selecione a Rodada</option>
+              {EnumRodada.map((numeroRodada, index) => (
+                <option key={index} value={numeroRodada}>{numeroRodada}</option>
+              ))}
+            </select>
+          </div>
+          <div className="flex items-end justify-center">
+            <button
+              type="submit"
+              className="btn-gradient2 py-4 px-4 rounded-3xl hover:scale-105 duration-300 ease-in-out"
+            >
+              <Swords size={30} strokeWidth={1.5} />
+            </button>
+          </div>
+        </form>
+      )}
+      <div className='flex flex-col justify-center items-center mt-4'>
+        <ErrorDisplay />
+      </div>
+      {data && (
+        <TableGrupos data={data} />
+      )}
     </div>
   )
 }
--- frontend/src/components/TableGrupos/index.tsx
+++ frontend/src/components/TableGrupos/index.tsx
@@ -1,27 +1,21 @@
-import { IPlayerDataProps } from '../../types/playerType'
 
-const ParticipantsTable = ({ dadosJSON }: { dadosJSON: IPlayerDataProps[]}) => {
-
+const TableGrupos = ({ data }: { data: string[] }) => {
   return (
     <section className='flex flex-col items-center justify-center'>
-      <h2 className='text-2xl text-center font-caustenBd mb-4'>Todos os Jogadores Importados:</h2>
+      <h2 className='text-2xl text-gray-200 text-center font-caustenBd mb-4'>Grupo da Rodada:</h2>
       <div className="relative shadow-lg sm:rounded-lg">
         <table className="w-full text-lg text-left rtl:text-right text-gray-200 shadow-xl">
           <thead className="text-sm uppercase bg-[#2D2D2F] text-gray-300">
             <tr className='text-center'>
               <th scope="col" className="px-6 py-3">
-                        Game Name
-              </th>
-              <th scope="col" className="px-6 py-3">
-                        Discord ID
+                Game Name
               </th>
             </tr>
           </thead>
           <tbody>
-            {dadosJSON.map((item: IPlayerDataProps, index: number) => (
+            {data.map((item: string, index: number) => (
               <tr key={index} className=" odd:bg-[#344981] even:bg-[#2D2D2F] border-b border-gray-700">
-                <td className="px-6 py-4">{item.inGameName}</td>
-                <td className="px-6 py-4">{item.discordID}</td>
+                <td className="px-6 py-4">{item}</td>
               </tr>
             ))}
           </tbody>
@@ -31,4 +25,4 @@ const ParticipantsTable = ({ dadosJSON }: { dadosJSON: IPlayerDataProps[]}) => {
   )
 }
 
-export default ParticipantsTable
+export default TableGrupos
